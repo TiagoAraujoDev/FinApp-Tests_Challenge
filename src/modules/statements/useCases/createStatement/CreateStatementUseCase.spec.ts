2bcba5276989996d485ac145ec10fbf67b9297be
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-09-10 13:21:45 -0300

    Finished: tests

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -1,10 +1,10 @@
-import { stringify } from "ts-jest";
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository"
 import { AuthenticateUserUseCase } from "../../../users/useCases/authenticateUser/AuthenticateUserUseCase";
 import { CreateUserUseCase } from "../../../users/useCases/createUser/CreateUserUseCase";
 import { ICreateUserDTO } from "../../../users/useCases/createUser/ICreateUserDTO";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
 import { OperationType } from "./CreateStatementController";
+import { CreateStatementError } from "./CreateStatementError";
 import { CreateStatementUseCase } from "./CreateStatementUseCase";
 import { ICreateStatementDTO } from "./ICreateStatementDTO";
 
@@ -12,13 +12,11 @@ let inMemoryUsersRepository: InMemoryUsersRepository;
 let inMemoryStatementsRepository: InMemoryStatementsRepository;
 let createStatementUseCase: CreateStatementUseCase;
 let createUserUseCase: CreateUserUseCase;
-let authenticateUserUseCase: AuthenticateUserUseCase;
 
 describe("Create Statement", () => {
   beforeEach(() => {
     inMemoryUsersRepository = new InMemoryUsersRepository();
     inMemoryStatementsRepository = new InMemoryStatementsRepository();
-    authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository);
     createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
     createStatementUseCase = new CreateStatementUseCase(
       inMemoryUsersRepository,
@@ -52,14 +50,79 @@ describe("Create Statement", () => {
   });
 
   it("Should be able to create a statement of withdraw", async () => {
-    // TODO
+    const userData: ICreateUserDTO = {
+      name: "Mr. Tiago",
+      email: "tiago@test.com",
+      password: "asdf"
+    }
+    
+    const user = await createUserUseCase.execute(userData);
+
+    const depositStatementData: ICreateStatementDTO = {
+      user_id: user.id as string,
+      type: OperationType.DEPOSIT,
+      amount: 10000,
+      description: "Money to buy something pretty",
+    }
+
+    await createStatementUseCase.execute(depositStatementData);
+
+    const withdrawStatementData: ICreateStatementDTO = {
+      user_id: user.id as string,
+      type: OperationType.WITHDRAW,
+      amount: 10000,
+      description: "Money to buy something pretty",
+    }
+
+    const withdrawStatement = await createStatementUseCase.execute(withdrawStatementData);
+
+    expect(withdrawStatement).toHaveProperty("id");
+    expect(withdrawStatement.amount).toEqual(10000);
+    expect(withdrawStatement.user_id).toEqual(user.id);
+    expect(withdrawStatement.description).toEqual("Money to buy something pretty");
+    expect(withdrawStatement).toHaveProperty("type", 'withdraw');
   });
 
   it("Should not be able to create a statement of a non existen user", async () => {
-    // TODO
+    const depositStatementData: ICreateStatementDTO = {
+      user_id: "98234720397849",
+      type: OperationType.DEPOSIT,
+      amount: 10000,
+      description: "Money to buy something pretty",
+    }
+
+    await expect(
+      createStatementUseCase.execute(depositStatementData)
+    ).rejects.toEqual(new CreateStatementError.UserNotFound());
   });
 
-  it("Should not be able to create a statement if insuficient founds", async () => {
-    // TODO
+  it("Should not be able to create a withdraw statement with insufficient founds", async () => {
+    const userData: ICreateUserDTO = {
+      name: "Steve Rogers",
+      email: "godheart@marvel.com",
+      password: "1234"
+    }
+    
+    const user = await createUserUseCase.execute(userData);
+
+    const depositStatementData: ICreateStatementDTO = {
+      user_id: user.id as string,
+      type: OperationType.DEPOSIT,
+      amount: 100,
+      description: "Money to buy something pretty",
+    }
+
+    await createStatementUseCase.execute(depositStatementData);
+
+    const withdrawStatementData: ICreateStatementDTO = {
+      user_id: user.id as string,
+      type: OperationType.WITHDRAW,
+      amount: 101,
+      description: "Money to buy something pretty",
+    }
+
+    await expect(
+      createStatementUseCase.execute(withdrawStatementData)
+    ).rejects.toEqual(new CreateStatementError.InsufficientFunds());
   });
 });
